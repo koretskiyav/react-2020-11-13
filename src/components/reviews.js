fa76c879b3952a2eef2821294da4e1081978ad27
Mykyta Simogin <mykita.simogin@evia.de>
2020-11-18 16:02:11 +0100

    key for reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -5,7 +5,7 @@ export default function Reviews(props) {
   return (
     <div>
       {props.reviews.map((review) => (
-        <Review review={review} />
+        <Review review={review} key={review.id} />
       ))}
     </div>
   );
