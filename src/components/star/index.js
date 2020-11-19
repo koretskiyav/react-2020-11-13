a593acc39810065f95e26081da5e2b9cb17298f1
Roman Ershov <romma4893@gmail.com>
2020-11-19 09:51:40 +0300

    added fullness of star

--- src/components/star/index.js
+++ src/components/star/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import propTypes from 'prop-types';
 import './star.css';
 
-const Star = ({ selected = false }) => {
-  const _classList = `star ${selected ? 'star_selected' : ''}`;
+const Star = ({ fullness = 0 }) => {
+  const _starSelectedStyle = {
+    width: `${fullness * 100}%`,
+  };
 
-  return <div className={_classList}></div>;
+  return (
+    <div className="star">
+      <div className="star_selected" style={_starSelectedStyle}></div>
+    </div>
+  );
 };
 
 Star.propTypes = {
-  selected: propTypes.bool,
+  fullness: (props, propName, componentName) => {
+    const prop = props[propName];
+
+    if (typeof prop !== 'number' || prop > 1 || prop < 0) {
+      return new Error(
+        `Проп \`${propName}\` компонента \`${componentName}\` имеет неправильное значение`
+      );
+    }
+  },
 };
 
 export default Star;
