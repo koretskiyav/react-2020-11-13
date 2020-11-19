08392a51506c05fce6a00da297c19936e97b94f6
Roman Ershov <romma4893@gmail.com>
2020-11-19 10:21:02 +0300

    added component rate

---
+++ src/components/rate/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Star from '../star';
+import './rate.css';
+
+const Rate = ({ rating = 0 }) => {
+  const wholeStar = Math.floor(rating);
+  const rest = rating - wholeStar;
+
+  return (
+    <div className="rate">
+      {[...Array(5)].map((_, i) => (
+        <Star
+          key={i}
+          fullness={i < wholeStar ? 1 : i + rest === rating ? rest : 0}
+        />
+      ))}
+    </div>
+  );
+};
+
+Rate.propTypes = {
+  rating: (props, propName, componentName) => {
+    const prop = props[propName];
+
+    if (typeof prop !== 'number' || prop > 5 || prop < 0) {
+      return new Error(
+        `Проп \`${propName}\` компонента \`${componentName}\` имеет неправильное значение`
+      );
+    }
+  },
+};
+
+export default Rate;
