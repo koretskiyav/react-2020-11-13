7dadca2ae77f63edde8a86e94482543da43fb032
Valentyn S. Uskov <just.valentine.now@gmail.com>
2020-11-22 20:36:07 +0300

    Add decrement test to product

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -26,6 +26,7 @@ describe('Product', () => {
 
   it('should decrement amount', () => {
     const wrapper = mount(<Product product={product} />);
+    wrapper.find('[data-id="product-increment"]').simulate('click');
     wrapper.find('[data-id="product-decrement"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
   });
