a50e88f549c284906fe46b8159a925d3561a0249
antonnekrasov <avnekrasov85@gmail.com>
2020-11-22 14:25:15 +0300

    [ht2] added proptypes

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Review from './review';
+import PropTypes from 'prop-types';
 import styles from './reviews.module.css';
 
 const Reviews = ({ reviews }) => {
@@ -10,6 +11,14 @@ const Reviews = ({ reviews }) => {
       ))}
     </div>
   );
+};
+
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default Reviews;
