845285f511fb24b52a1cb6a34bed4e494076a910
Roman Ershov <romma4893@gmail.com>
2020-11-25 17:49:38 +0300

    fix negative amount

--- src/redux/reducer/order.js
+++ src/redux/reducer/order.js
@@ -2,12 +2,15 @@ import { DECREMENT, INCREMENT } from '../constants';
 
 // { [productId]: amount }
 const reducer = (state = {}, action) => {
-  const { type, payload } = action;
+  const { type, payload = {} } = action;
+  const { id: productId } = payload;
+  const productAmount = state[productId] || 0;
+
   switch (type) {
     case INCREMENT:
-      return { ...state, [payload.id]: (state[payload.id] || 0) + 1 };
+      return { ...state, [productId]: productAmount + 1 };
     case DECREMENT:
-      return { ...state, [payload.id]: (state[payload.id] || 0) - 1 };
+      return { ...state, [productId]: Math.max(productAmount - 1, 0) };
     default:
       return state;
   }
