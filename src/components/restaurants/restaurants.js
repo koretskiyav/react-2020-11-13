d12ec4f280ea3963ea1c089e122f54c2054859ee
Bondar <bondardevelop@gmail.com>
2020-11-26 23:47:26 +0200

    HT3 create order component

--- src/components/restaurants/restaurants.js
+++ src/components/restaurants/restaurants.js
@@ -2,6 +2,7 @@ import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Restaurant from '../restaurant';
 import Navigation from '../navigation';
+import Order from '../order';
 
 const Restaurants = ({ restaurants }) => {
   const [activeRestaurantId, setActiveRestaurant] = useState(restaurants[0].id);
@@ -16,6 +17,10 @@ const Restaurants = ({ restaurants }) => {
       <Navigation
         restaurants={restaurants}
         onRestaurantClick={setActiveRestaurant}
+      />
+      <Order
+        restaurants={restaurants}
+        activeRestaurantId={activeRestaurantId}
       />
       <Restaurant restaurant={activeRestaurant} />
     </div>
