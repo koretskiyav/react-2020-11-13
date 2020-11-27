c479cfa3521df5c1c82cd729142a57a096dbb53c
andriiko <andriiko@wix.com>
2020-11-27 21:03:09 +0200

    add logger middleware

---
+++ src/redux/middleware/logger.js
@@ -0,0 +1,8 @@
+const logger = (store) => (next) => (action) => {
+  console.log('before: ', store.getState());
+  console.log('action: ', action);
+  next(action);
+  console.log('after: ', store.getState());
+};
+
+export default logger;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from './middleware/logger';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 export default store;
