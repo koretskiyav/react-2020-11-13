98fa1983a11dd643985e2607d21b5c7c3b0f7cfa
Vadim <borissowadim@mail.ru>
2020-11-28 23:07:35 +0100

    correction bug id gen for all actions

--- src/redux/middleware/uuid.js
+++ src/redux/middleware/uuid.js
@@ -1,12 +1,20 @@
 import { v4 as uuidv4 } from 'uuid';
+import { SUBMIT } from '../constants';
 
 const uuid = (store) => (next) => (action) => {
   const {
     payload: { values },
+    type,
   } = action;
 
-  const valuesWithId = { ...values, id: uuidv4(), userId: uuidv4() };
-  next({ ...action, payload: valuesWithId });
+  switch (type) {
+    case SUBMIT:
+      const valuesWithId = { ...values, id: uuidv4(), userId: uuidv4() };
+      next({ ...action, payload: valuesWithId });
+      break;
+    default:
+      next(action);
+  }
 };
 
 export default uuid;
