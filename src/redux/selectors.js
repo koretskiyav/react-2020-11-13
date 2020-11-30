bb199bfcc1cc1ccd1b18e6ebf70bad0db672f68c
Kostiantyn Polishchuk <pol_kos@ukr.net>
2020-11-30 20:03:14 +0200

    fix average rating

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,6 +3,8 @@ import { createSelector } from 'reselect';
 const restaurantsSelector = (state) => state.restaurants;
 const orderSelector = (state) => state.order;
 const productsSelector = (state) => state.products;
+const reviewsSelector = (state) => state.reviews;
+const restaurantReviewsSelector = (state, restaurantId) => state.restaurants[restaurantId].reviews;
 
 export const restaurantListSelector = createSelector(
   restaurantsSelector,
@@ -28,4 +30,16 @@ export const totalSelector = createSelector(
   orderProductsSelector,
   (orderProducts) =>
     orderProducts.reduce((acc, { subtotal }) => acc + subtotal, 0)
+);
+
+export const makeRatingSelector = () => createSelector(
+  reviewsSelector,
+  restaurantReviewsSelector,
+  (reviews, restaurantReviews) => {
+    const total = restaurantReviews
+      .map(id => reviews[id])
+      .reduce((acc, { rating }) => acc + rating, 0);
+
+    return Math.round(total / restaurantReviews.length);
+  }
 );
