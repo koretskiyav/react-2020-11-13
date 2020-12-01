5a1194e2eb62dc2306646f6fd67c56ad0550fa8a
andriiko <andriiko@wix.com>
2020-12-01 20:55:23 +0200

    add loading state

--- src/redux/middleware/api.js
+++ src/redux/middleware/api.js
@@ -1,11 +1,17 @@
+import { REQUEST, SUCCESS, FAILURE } from '../constants';
+
 const api = (store) => (next) => async (action) => {
   if (!action.CallAPI) return next(action);
 
-  const { CallAPI, ...rest } = action;
+  const { CallAPI, type, ...rest } = action;
 
-  const response = await fetch(CallAPI).then((res) => res.json());
-
-  next({ ...rest, response });
+  next({ ...rest, type: type + REQUEST });
+  try {
+    const response = await fetch(CallAPI).then((res) => res.json());
+    next({ ...rest, type: type + SUCCESS, response });
+  } catch (error) {
+    next({ ...rest, type: type + FAILURE, error });
+  }
 };
 
 export default api;
--- src/redux/reducer/restaurants.js
+++ src/redux/reducer/restaurants.js
@@ -1,27 +1,48 @@
 import produce from 'immer';
-import { ADD_REVIEW, LOAD_RESTAURANTS } from '../constants';
+import {
+  ADD_REVIEW,
+  FAILURE,
+  LOAD_RESTAURANTS,
+  REQUEST,
+  SUCCESS,
+} from '../constants';
 import { arrToMap } from '../utils';
 
-const reducer = (state = {}, action) => {
-  const { type, payload, reviewId, response } = action;
+const initialState = {
+  entities: {},
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, payload, reviewId, response, error } = action;
 
   switch (type) {
-    case LOAD_RESTAURANTS:
-      return arrToMap(response);
+    case LOAD_RESTAURANTS + REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case LOAD_RESTAURANTS + SUCCESS:
+      return {
+        ...state,
+        entities: arrToMap(response),
+        loading: false,
+        loaded: true,
+      };
+    case LOAD_RESTAURANTS + FAILURE:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error,
+      };
     case ADD_REVIEW:
       return produce(state, (draft) => {
-        draft[payload.restaurantId].reviews.push(reviewId);
+        draft.entities[payload.restaurantId].reviews.push(reviewId);
       });
-    // case ADD_REVIEW:
-    //   const restaurant = state[payload.restaurantId];
-    //   return {
-    //     ...state,
-    //     [payload.restaurantId]: {
-    //       ...restaurant,
-    //       reviews: [...restaurant.reviews, reviewId],
-    //     },
-    //   };
-
     default:
       return state;
   }
