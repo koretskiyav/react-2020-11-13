7feb8879bbe4869054c1bd40e9b3c16fea8bc758
andriiko <andriiko@wix.com>
2020-12-04 16:50:41 +0200

    add HW5

--- src/redux/reducer/products.js
+++ src/redux/reducer/products.js
@@ -1,14 +1,38 @@
-import { normalizedProducts } from '../../fixtures';
+import produce from 'immer';
 import { arrToMap } from '../utils';
+import { LOAD_PRODUCTS, REQUEST, SUCCESS, FAILURE } from '../constants';
 
-// { [productId]: product }
-const reducer = (state = arrToMap(normalizedProducts), action) => {
-  const { type } = action;
-
-  switch (type) {
-    default:
-      return state;
-  }
+const initialState = {
+  loading: {},
+  loaded: {},
+  error: null,
+  entities: {},
 };
 
+const reducer = (state = initialState, action) =>
+  produce(state, (draft) => {
+    const { type, response, error, restaurantId } = action;
+
+    switch (type) {
+      case LOAD_PRODUCTS + REQUEST: {
+        draft.loading[restaurantId] = true;
+        break;
+      }
+      case LOAD_PRODUCTS + SUCCESS: {
+        draft.loading[restaurantId] = false;
+        draft.loaded[restaurantId] = true;
+        draft.error = null;
+        draft.entities = { ...draft.entities, ...arrToMap(response) };
+        break;
+      }
+      case LOAD_PRODUCTS + FAILURE: {
+        draft.loading[restaurantId] = false;
+        draft.loaded[restaurantId] = false;
+        draft.error = error;
+        break;
+      }
+      default:
+        return;
+    }
+  });
 export default reducer;
