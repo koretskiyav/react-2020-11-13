225143b04c145025dfe68384942ba2310ba8c3f7
antonnekrasov <avnekrasov85@gmail.com>
2020-12-06 19:26:45 +0300

    [ht6] routes to menu & reviews

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import cn from 'classnames';
-
 import styles from './tabs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Tabs = ({ tabs }) => {
-  const [activeTab, setActiveTab] = useState(0);
-
-  const { content } = tabs[activeTab];
-
   return (
     <>
       <div className={styles.tabs}>
-        {tabs.map(({ title }, index) => (
-          <span
-            key={title}
-            className={cn(styles.tab, { [styles.active]: index === activeTab })}
-            onClick={() => setActiveTab(index)}
-          >
-            {title}
-          </span>
-        ))}
+        {tabs.map(({ id, title, to }) => {
+          return (
+            <NavLink
+              key={id}
+              to={to}
+              className={styles.tab}
+              activeClassName={styles.active}
+            >
+              {title}
+            </NavLink>
+          );
+        })}
       </div>
-      {content}
     </>
   );
 };
@@ -31,7 +28,6 @@ Tabs.propTypes = {
   tabs: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
-      content: PropTypes.element.isRequired,
     }).isRequired
   ).isRequired,
 };
