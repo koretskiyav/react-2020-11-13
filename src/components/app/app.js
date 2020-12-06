3bb5c990c41a2ba207d2a0f17ebb261fa12dfdbc
veax <borissowadim@mail.ru>
2020-12-06 22:17:26 +0100

    lesson_6 remove routes from app js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,8 +2,6 @@ import React, { PureComponent } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../header';
 import Basket from '../basket';
-import Menu from '../menu';
-import Review from '../reviews/review';
 import RestaurantsPage from '../../pages/restaurants-page';
 
 export default class App extends PureComponent {
@@ -15,8 +13,6 @@ export default class App extends PureComponent {
           <Route path="/" exact component={() => 'Main page'} />
           <Route path="/checkout" component={Basket} />
           <Route path="/restaurants" component={RestaurantsPage} />
-          <Route path="/restaurants/:id/menu" exact component={Menu} />
-          <Route path="/restaurants/:id/reviews" exact component={Review} />
           <Route path="/" component={() => '404 - not found'} />
         </Switch>
       </div>
