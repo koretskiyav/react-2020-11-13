a1fb1c136bb817f61d77bdf7fe04e993a8c1408f
veax <borissowadim@mail.ru>
2020-12-10 21:29:35 +0100

    lesson_7 add redirects

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Header from '../header';
 import Basket from '../basket';
 import RestaurantsPage from '../../pages/restaurants-page';
@@ -12,6 +12,7 @@ const App = () => {
       <UserProvider value={{ name, setName }}>
         <Header />
         <Switch>
+          <Redirect exact from="/" to="/restaurants" />
           <Route path="/checkout" component={Basket} />
           <Route path="/restaurants" component={RestaurantsPage} />
           <Route path="/error" component={() => <h1>Error Page</h1>} />
