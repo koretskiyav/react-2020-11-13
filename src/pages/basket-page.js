7f5b0a7008c4fe6136b43a93f31877a282688165
Kostiantyn Polishchuk <pol_kos@ukr.net>
2020-12-11 06:01:28 +0200

    add css for basket page

--- src/pages/basket-page.js
+++ src/pages/basket-page.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 
+import styles from './basket-page.module.css';
+
 import Basket from '../components/basket';
 import { orderErrorSelector } from '../redux/selectors';
 
@@ -15,10 +17,10 @@ const BasketPage = ({ match, orderError }) => {
         <Basket canCheckout />
       </Route>
       <Route path={`${path}/error`}>
-        <h2 style={{textAlign: 'center', marginTop: 100}}>{orderError}</h2>
+        <h2 className={styles.message}>{orderError}</h2>
       </Route>
       <Route path={`${path}/success`}>
-        <h2 style={{textAlign: 'center', marginTop: 100}}>Thanks for your order!</h2>
+        <h2 className={styles.message}>Thanks for your order!</h2>
       </Route>
     </Switch>
   )
